efe85f1515694cb7f771e2dcade074ff78188518
boyfromthewell <tnsdyd10@naver.com>
2025-10-10 23:56:38 +0900

    feat: 프로젝트 초기 세팅

---
+++ types/index.ts
@@ -0,0 +1,11 @@
+interface BlogPost {
+  slug: string;
+  title: string;
+  date: string;
+  description: string;
+  author: string;
+  thumbnail: string;
+  content: string;
+}
+
+export type { BlogPost };
